fdfefce77375eedb9cdc8e9b7632ccd01143f019
xlyann <gdhfufigj@gmail.com>
2024-07-16 23:03:24 +0900

    Title: 3 또는 5의 배수, Time: 113ms, Memory: 24MB, Status: Passed - CodeTree

--- 240716/3 또는 5의 배수/multiples-of-3-or-5.py
+++ 240716/3 또는 5의 배수/multiples-of-3-or-5.py
@@ -1,9 +1,9 @@
 a = int(input())
 if a%3 == 0:
-    print('Yes')
+    print('YES')
 else:
-    print('No')
+    print('NO')
 if a%5 == 0:
-    print('Yes')
+    print('YES')
 else:
-    print('No')
+    print('NO')
