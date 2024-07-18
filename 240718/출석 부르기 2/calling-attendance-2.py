1c9d571e8cb7618eedea9aebb43bc5c01ff4d810
xlyann <gdhfufigj@gmail.com>
2024-07-18 23:35:42 +0900

    Title: 출석 부르기 2, Time: 102ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240718/출석 부르기 2/calling-attendance-2.py
@@ -0,0 +1,13 @@
+while True:
+    a = int(input())
+    if a == 1:
+        print('John')
+    elif a == 2:
+        print('Tom')
+    elif a == 3:
+        print('Paul')
+    elif a == 4:
+        print('Sam')
+    else:
+        print('Vacancy')
+        break
