ce134a444aaaa00725f5f5bb1ac847ee4c8e7202
xlyann <gdhfufigj@gmail.com>
2024-07-20 15:56:32 +0900

    Title: 별표 출력하기 12, Time: 143ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240720/별표 출력하기 12/print-star-12.py
@@ -0,0 +1,8 @@
+n = int(input())
+for i in range(n):
+    for j in range(n):
+        if i == 0 or (j%2 == 1 and i<=j):
+            print('*', end = ' ')
+        else:
+            print(' ', end = ' ')
+    print()
