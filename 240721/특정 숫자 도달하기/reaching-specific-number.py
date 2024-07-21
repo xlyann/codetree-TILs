44f2650b04e0dd6d12133bb4f2a18635527fdddf
xlyann <gdhfufigj@gmail.com>
2024-07-21 22:44:53 +0900

    Title: 특정 숫자 도달하기, Time: 124ms, Memory: 24MB, Status: Passed - CodeTree

--- 240721/특정 숫자 도달하기/reaching-specific-number.py
+++ 240721/특정 숫자 도달하기/reaching-specific-number.py
@@ -1,8 +1,11 @@
 a = list(map(int, input().split()))
+c = True
 
 for i in a:
     if i >= 250:
         b = a.index(i)
+        c = False
         break
-a = a[:-(len(a)-b)]
+if c == False:
+    a = a[:-(len(a)-b)]
 print(sum(a), '{:.1f}'.format(sum(a)/len(a)))
