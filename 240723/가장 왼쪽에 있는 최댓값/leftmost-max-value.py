e525022c816efc946f43793987d172e9ec9e9f1d
xlyann <gdhfufigj@gmail.com>
2024-07-23 00:59:01 +0900

    Title: 가장 왼쪽에 있는 최댓값, Time: 159ms, Memory: 25MB, Status: Passed - CodeTree

--- 240723/가장 왼쪽에 있는 최댓값/leftmost-max-value.py
+++ 240723/가장 왼쪽에 있는 최댓값/leftmost-max-value.py
@@ -1,12 +1,11 @@
 a = int(input())
 num = list(map(int, input().split()))
-cut = a-1
-while True:
-    new = num[:cut]
-    if new == []:
-        break
+cut = 0
+new = num[:]
+while new != []:
     _max = max(new)
     find = num.index(_max)
     
     print(find+1, end = ' ')
     cut = find-a
+    new = num[:cut]
