fd7da7daf598712cfb3147bf511e73b0c91add84
xlyann <gdhfufigj@gmail.com>
2024-07-24 23:05:27 +0900

    Title: 격자에 점 그리기, Time: 126ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240724/격자에 점 그리기/draw-points-on-grid.py
@@ -0,0 +1,11 @@
+n, m = map(int, input().split())
+new = [[0]*n for _ in range(n)]
+
+for _ in range(m):
+    r, c = map(int, input().split())
+    new[r-1][c-1] = _+1
+
+for row in new:
+    for e in row:
+        print(e, end = ' ')
+    print()
