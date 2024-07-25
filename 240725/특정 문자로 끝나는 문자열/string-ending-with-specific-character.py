f7e2d52d451793ff7cf3bd6180d03ba94ec1af74
xlyann <gdhfufigj@gmail.com>
2024-07-25 21:59:13 +0900

    Title: 특정 문자로 끝나는 문자열, Time: 141ms, Memory: 24MB, Status: Passed - CodeTree

--- 240725/특정 문자로 끝나는 문자열/string-ending-with-specific-character.py
+++ 240725/특정 문자로 끝나는 문자열/string-ending-with-specific-character.py
@@ -1,5 +1,11 @@
 _list = list(input() for _ in range(10))
 find = input()
+count = 0
 for i in _list:
     if i[-1] == find:
         print(i)
+    else:
+        count += 1
+
+if count == 10:
+    print('None')
