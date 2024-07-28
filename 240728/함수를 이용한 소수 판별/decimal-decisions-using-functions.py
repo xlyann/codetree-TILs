f8193893c517ffb67abe74b45bd5e1095968bb44
xlyann <gdhfufigj@gmail.com>
2024-07-28 15:33:15 +0900

    Title: 함수를 이용한 소수 판별, Time: 111ms, Memory: 24MB, Status: Passed - CodeTree

--- 240728/함수를 이용한 소수 판별/decimal-decisions-using-functions.py
+++ 240728/함수를 이용한 소수 판별/decimal-decisions-using-functions.py
@@ -2,6 +2,8 @@ def prime(num):
     for i in range(2, int(num**0.5)+1):
         if num % i == 0:
             return False
+    if num == 1:
+        return False
     return True
 
 def prime_sum(a, b):
