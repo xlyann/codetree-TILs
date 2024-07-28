cfde3687631a8ad9448c689e41d5fd0d61c22134
xlyann <gdhfufigj@gmail.com>
2024-07-28 15:25:34 +0900

    Title: 함수를 이용한 369 게임, Time: 187ms, Memory: 29MB, Status: Wrong Answer - CodeTree

---
+++ 240728/함수를 이용한 369 게임/369-games-using-functions.py
@@ -0,0 +1,18 @@
+def in_3(num):
+    num = str(num)
+    for i in num:
+        if int(i)%3 == 0:
+            return True
+    return False
+
+def mul_3(num):
+    return num%3 == 0
+
+count = 0
+a, b = map(int, input().split())
+
+for i in range(a, b+1):
+    if in_3(i) or mul_3(i):
+        count += 1
+
+print(count)
