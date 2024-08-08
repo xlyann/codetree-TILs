b91f217dd0b567d983463dd2174f0c117f730bc3
xlyann <gdhfufigj@gmail.com>
2024-08-08 22:12:05 +0900

    Title: 블럭쌓는 명령2, Time: 156ms, Memory: 25MB, Status: Passed - CodeTree

--- 240808/블럭쌓는 명령2/block-stacking-commands2.py
+++ 240808/블럭쌓는 명령2/block-stacking-commands2.py
@@ -1,8 +1,8 @@
 N, K = map(int, input().split())
-block = [0]*N
+block = [0]*(N+1)
 for j in range(K):
     init, final = map(int, input().split())
     for i in range(init, final + 1):
-        block[i-1] += 1
+        block[i] += 1
 
 print(max(block))
