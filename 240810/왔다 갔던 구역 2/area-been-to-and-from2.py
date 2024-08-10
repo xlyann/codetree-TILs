c58dd58994b0a886466ac7d1b411f5812997c0b2
xlyann <gdhfufigj@gmail.com>
2024-08-10 23:16:46 +0900

    Title: 왔다 갔던 구역 2, Time: 114ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240810/왔다 갔던 구역 2/area-been-to-and-from2.py
@@ -0,0 +1,32 @@
+line = []
+save = []
+n = int(input())
+for i in range(n):
+    num, LorR = input().split()
+    a = (int(num), LorR)
+    save.append(a)
+
+point = 0
+for a, b in save:
+    if b == 'R':
+        if point + a > len(line):
+            line = line + [0]*(point + a - len(line))
+        
+        for i in range(point, point + a):
+            line[i] += 1
+        
+        point += a
+    else:
+        if point - a < 0:
+            line = [0]*(abs(point-a)) + line
+        
+        for i in range(a):
+            line[i] += 1
+        
+        point = 0
+    
+count = 0
+for over_2 in line:
+    if over_2 >= 2:
+        count += 1
+print(count)
