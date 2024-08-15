fe3785c0d9da1648bd56fde8bee2de741fad8b31
xlyann <gdhfufigj@gmail.com>
2024-08-15 23:00:43 +0900

    Title: 연속되는 수 2, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240815/연속되는 수 2/continuous-number2.py
@@ -0,0 +1,16 @@
+n = int(input())
+_list = list(int(input()) for _ in range(n))
+save = []
+
+count = 1
+for i in range(n):
+    if i == 0:
+        continue
+    
+    if _list[i] != _list[i-1]:
+        save.append(count) 
+        count = 1
+    else:
+        count += 1
+
+print(max(save))
