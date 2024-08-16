d1f3b92f01d595983ce398da9ef7f9e3cdbd221d
xlyann <gdhfufigj@gmail.com>
2024-08-16 22:08:04 +0900

    Title: 선두를 지켜라, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240816/선두를 지켜라/keep-the-lead.py
@@ -0,0 +1,32 @@
+times_a, times_b = map(int, input().split())
+move_a = [0]
+move_b = [0]
+
+for i in range(times_a):
+    v, t = map(int, input().split())
+    for _ in range(t):
+        move_a.append(move_a[-1] + v)
+
+for i in range(times_b):
+    v, t = map(int, input().split())
+    for _ in range(t):
+        move_b.append(move_b[-1] + v)
+
+
+cnt = 0
+
+for i in range(1, len(move_a)):
+    if move_a[i] == move_b[i]:
+        continue
+    
+    if i == 1:
+        old = 0
+    
+    new = move_a[i] - move_b[i]
+
+    if old * new < 0:
+        cnt += 1
+    
+    old = new
+
+print(cnt)
