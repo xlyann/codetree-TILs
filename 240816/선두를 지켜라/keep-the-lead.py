ef6922db064281af65ba52614f8ddd53b5403ec6
xlyann <gdhfufigj@gmail.com>
2024-08-16 22:10:26 +0900

    Title: 선두를 지켜라, Time: 205ms, Memory: 39MB, Status: Passed - CodeTree

--- 240816/선두를 지켜라/keep-the-lead.py
+++ 240816/선두를 지켜라/keep-the-lead.py
@@ -12,16 +12,13 @@ for i in range(times_b):
     for _ in range(t):
         move_b.append(move_b[-1] + v)
 
-
 cnt = 0
+old = 0
 
 for i in range(1, len(move_a)):
     if move_a[i] == move_b[i]:
         continue
-    
-    if i == 1:
-        old = 0
-    
+        
     new = move_a[i] - move_b[i]
 
     if old * new < 0:
