8eb78e56d91e4c160a1500d519570db7f9d5e578
xlyann <gdhfufigj@gmail.com>
2024-08-16 21:43:00 +0900

    Title: 만나는 그 순간, Time: 110ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240816/만나는 그 순간/the-moment-we-meet.py
@@ -0,0 +1,31 @@
+start = 0
+times_a, times_b = map(int, input().split())
+move_a = [0]
+move_b = [0]
+
+def move(num, ab):
+    for i in range(num):
+        LR, distance = input().split()
+        distance = int(distance)
+
+        if LR == 'L':
+            stand = -1
+        else:
+            stand = 1
+
+        if ab == 'a':
+            for _ in range(distance):
+                move_a.append(move_a[-1] + stand)
+        else:
+            for _ in range(distance):
+                move_b.append(move_b[-1] + stand)
+
+move(times_a, 'a')
+move(times_b, 'b')
+
+limit = min(len(move_a), len(move_b))
+
+for i in range(1, limit):
+    if move_a[i] == move_b[i]:
+        print(i)
+        break
