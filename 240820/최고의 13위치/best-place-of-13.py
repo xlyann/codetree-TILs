70bab30763ed9dd05ea0ea9556d4a4fec79137f6
xlyann <gdhfufigj@gmail.com>
2024-08-20 17:09:34 +0900

    Title: 최고의 13위치, Time: 131ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240820/최고의 13위치/best-place-of-13.py
@@ -0,0 +1,9 @@
+max_cnt = 0
+n = int(input())
+arr = [list(map(int, input().split())) for _ in range(n)]
+
+for i in range(n):
+    for j in range(n-2):
+        max_cnt = max(max_cnt, arr[i][j] + arr[i][j+1] + arr[i][j+2])
+
+print(max_cnt)
