6ffb2b9ff2e7d3e0ccb5d27c770b955fef6d174a
xlyann <gdhfufigj@gmail.com>
2024-08-22 02:07:53 +0900

    Title: 오목, Time: 112ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240822/오목/O-mok.py
+++ 240822/오목/O-mok.py
@@ -2,12 +2,14 @@ arr = [list(map(int, input().split())) for _ in range(19)]
 winner = 0
 
 def win(a, b):
-    if a < 15 and b < 15:
-        return win_col(a, b) or win_row(a, b) or win_dia(a, b)
-    elif a >= 15 and b < 15:
-        return win_row(a, b) or win_dia(a, b)
-    elif a < 15 and b >= 15:
-        return win_col(a, b) or win_dia(a, b)
+    if b <= 14 and win_row(a, b):
+        return 1
+    if a >= 14 and win_col(a, b):
+        return 1
+    if a <= 14 and b >= 14 and win_dia_L(a, b):
+        return 1
+    if a>=4 and b>=4 and win_dia_R(a, b):
+        return 1
     return 0
 
 def win_row(a, b):
@@ -28,10 +30,19 @@ def win_col(a, b):
         return 1
     return 0
 
-def win_dia(a, b):
+def win_dia_L(a, b):
     _list = []
     for i in range(0, 5):
         _list.append(arr[a+i][b+i])
+    new = set(_list)
+    if len(new) == 1:
+        return 1
+    return 0
+
+def win_dia_R(a, b):
+    _list = []
+    for i in range(0, 5):
+        _list.append(arr[a-i][b+i])
     new = set(_list)
     if len(new) == 1:
         return 1
@@ -46,9 +57,12 @@ for i in range(19):
                 x, y = i, j+2
             elif win_col(i, j):
                 x, y = i+2, j
-            elif win_dia(i, j):
+            elif win_dia_L(i, j):
                 x, y = i+2, j+2
+            elif win_dia_R(i, j):
+                x, y = i-2, j+2
             break
 
 print(winner)
-print(x+1, y+1)
+if winner != 0:
+    print(x+1, y+1)
