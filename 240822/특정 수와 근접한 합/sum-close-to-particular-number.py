3673377aa1f31f23e61037c973e94fdd28f2eb15
xlyann <gdhfufigj@gmail.com>
2024-08-22 02:53:29 +0900

    Title: 특정 수와 근접한 합, Time: 118ms, Memory: 25MB, Status: Passed - CodeTree

--- 240822/특정 수와 근접한 합/sum-close-to-particular-number.py
+++ 240822/특정 수와 근접한 합/sum-close-to-particular-number.py
@@ -5,7 +5,7 @@ ans = 99999999
 sum_val = sum(num)
 
 for i in range(a):
-    for j in range(i+2, a):
+    for j in range(i+1, a):
         new = sum_val - num[i] - num[j]
         ans = min(ans, abs(b-new))
 
