09ba8848822571ae45ceb4960160e717ade94391
xlyann <gdhfufigj@gmail.com>
2024-08-22 02:42:02 +0900

    Title: 최고의 13위치 2, Time: 132ms, Memory: 25MB, Status: Passed - CodeTree

--- 240822/최고의 13위치 2/best-place-of-13-2.py
+++ 240822/최고의 13위치 2/best-place-of-13-2.py
@@ -5,7 +5,12 @@ arr = [list(map(int, input().split())) for _ in range(n)]
 for i in range(n):
     for j in range(n-2):
         for k in range(i, n):
-            for l in range(j+3, n-2):
-                max_cnt = max(max_cnt, arr[i][j] + arr[i][j+1] + arr[i][j+2] + arr[k][l] + arr[k][l+1] + arr[k][l+2])
+            if i == k:
+                for l in range(j+3, n-2):
+                    max_cnt = max(max_cnt, arr[i][j] + arr[i][j+1] + arr[i][j+2] + arr[k][l] + arr[k][l+1] + arr[k][l+2])
+            else:
+                for l in range(n-2):
+                    max_cnt = max(max_cnt, arr[i][j] + arr[i][j+1] + arr[i][j+2] + arr[k][l] + arr[k][l+1] + arr[k][l+2])
+                    
 
 print(max_cnt)
