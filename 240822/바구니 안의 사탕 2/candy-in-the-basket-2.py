332f34a852faebc13ceff86d80c991b1998d355d
xlyann <gdhfufigj@gmail.com>
2024-08-22 21:32:09 +0900

    Title: 바구니 안의 사탕 2, Time: 116ms, Memory: 25MB, Status: Passed - CodeTree

--- 240822/바구니 안의 사탕 2/candy-in-the-basket-2.py
+++ 240822/바구니 안의 사탕 2/candy-in-the-basket-2.py
@@ -1,13 +1,11 @@
 n, k = map(int, input().split())
-arr = [0]*101
+arr = [0]*1001
 candy = list(tuple(map(int, input().split())) for _ in range(n))
 for elem1, elem2 in candy:
     arr[elem2] += elem1
 
-current_sum = sum(arr[:2*k+2])
-max_sum = current_sum
-
-for i in range(1, 101 - 2*k):
-    current_sum = current_sum - arr[i-1] + arr[i + 2*k ]
-    max_sum = max(max_sum, current_sum)
-print(max_sum)
+ans = 0
+for i in range(k, 1001-k):
+    sum_val = sum(arr[i-k:i+k+1])
+    ans = max(ans, sum_val)
+print(ans)
