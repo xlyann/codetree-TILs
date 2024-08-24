4eea36375d98418f5e95c8e12f4f6503dd268edf
xlyann <gdhfufigj@gmail.com>
2024-08-24 23:17:57 +0900

    Title: 이상한 폭탄 2, Time: 147ms, Memory: 25MB, Status: Passed - CodeTree

--- 240824/이상한 폭탄 2/strange-bomb-2.py
+++ 240824/이상한 폭탄 2/strange-bomb-2.py
@@ -1,7 +1,7 @@
 n, k = map(int, input().split())
 bomb = list(int(input()) for _ in range(n))
 
-ans = 0
+ans = -1
 for i in range(n):
     for j in range(i-k, i+k):
         if 0 <= j and j<=n-1 and i != j and bomb[j] == bomb[i]:
