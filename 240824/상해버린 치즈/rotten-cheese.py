570c4e1d4259ff039db63e2679d6220117370a2b
xlyann <gdhfufigj@gmail.com>
2024-08-24 22:41:32 +0900

    Title: 상해버린 치즈, Time: 104ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240824/상해버린 치즈/rotten-cheese.py
@@ -0,0 +1,21 @@
+people, cheese, record, sick_record = map(int, input().split())
+eat = list(tuple(map(int, input().split())) for _ in range(record))
+sick = list(tuple(map(int, input().split())) for _ in range(sick_record))
+
+ans = 0
+for i in range(record):
+    pacient = [0]*(people+1)
+    stomach = [0]*(people+1)
+    guess = True
+    for who, num, second in eat:
+        if i == num:
+            pacient[who] = 1
+            stomach[who] = second+1
+    for who_sick, sick_time in sick:
+        if pacient[who_sick] == 1 and stomach[who_sick] <= sick_time:
+            continue
+        guess = False
+        break
+    if guess:
+        ans = max(ans, pacient.count(1))
+print(ans)
