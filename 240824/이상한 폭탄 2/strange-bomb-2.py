edbaa0a89e260206d72e08630c7d827c75bd27e4
xlyann <gdhfufigj@gmail.com>
2024-08-24 23:13:43 +0900

    Title: 이상한 폭탄 2, Time: 106ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240824/이상한 폭탄 2/strange-bomb-2.py
@@ -0,0 +1,9 @@
+n, k = map(int, input().split())
+bomb = list(int(input()) for _ in range(n))
+
+ans = 0
+for i in range(n):
+    for j in range(i-k, i+k):
+        if 0>=j and j<=n-1 and i!=j and bomb[j] == bomb[i]:
+            ans = max(ans, bomb[j])
+print(ans)
