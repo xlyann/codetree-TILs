62875a0ef89aec42ced8c7a867ce15137d322df4
xlyann <gdhfufigj@gmail.com>
2024-08-25 17:41:41 +0900

    Title: 등장하지 않는 문자열의 길이, Time: 215ms, Memory: 25MB, Status: Passed - CodeTree

--- 240825/등장하지 않는 문자열의 길이/length-of-string-that-does-not-appear.py
+++ 240825/등장하지 않는 문자열의 길이/length-of-string-that-does-not-appear.py
@@ -2,9 +2,9 @@ n = int(input())
 letter = list(input())
 
 ans = 0
-for i in range(1, n//2+1):
+for i in range(1, n):
     for j in range(n-i+1):
-        for k in range(j+i, n-i+1):
+        for k in range(j+1, n-i+1):
             if letter[j:j+i] == letter[k:k+i]:
                 ans = i
 print(ans+1)
