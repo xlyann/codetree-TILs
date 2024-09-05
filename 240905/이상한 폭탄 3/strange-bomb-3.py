c15bc603b61bb0659be2f4013539ac509cf914a1
xlyann <gdhfufigj@gmail.com>
2024-09-05 22:59:06 +0900

    Title: 이상한 폭탄 3, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240905/이상한 폭탄 3/strange-bomb-3.py
@@ -0,0 +1,22 @@
+n, k = map(int, input().split())
+num = list(int(input()) for _ in range(n))
+
+def in_range(a):
+    global n
+    return 0<=a and a<=n-1
+
+boom = {}
+ans = 0
+for i in range(n):
+    bomb = num[i]
+    for j in range(i-k, i+k+1):
+        if in_range(j) and i!=j and num[j] == bomb:
+            if bomb in boom:
+                boom[bomb] += 1
+            else:
+                boom[bomb] = 1
+            break
+
+new_boom = sorted(boom.items(), key = lambda x:(-x[1], -x[0]))
+
+print(new_boom[0][0])
