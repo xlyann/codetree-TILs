59b93df5f39b45afd0271b22f70b0179e1c2b4e6
xlyann <gdhfufigj@gmail.com>
2024-09-06 15:51:25 +0900

    Title: 구간 잘 나누기, Time: 140ms, Memory: 25MB, Status: Wrong Answer - CodeTree

--- 240906/구간 잘 나누기/divide-sections-well.py
+++ 240906/구간 잘 나누기/divide-sections-well.py
@@ -4,15 +4,15 @@ num = list(map(int, input().split()))
 for i in range(max(num), 5001):
     save = []
     limit = 0
-    for j in num:
-        if limit + j <= i:
-            limit += j
-            if j == num[-1]:
+    for j in range(n):
+        if limit + num[j] <= i:
+            limit += num[j]
+            if j == n-1:
                 save.append(limit)
         else:
             save.append(limit)
-            limit = j
-            if j == num[-1]:
+            limit = num[j]
+            if j == n-1:
                 save.append(j)
 
     if len(save) == m:
