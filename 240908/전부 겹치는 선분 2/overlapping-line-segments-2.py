a289729d71ccf6877f1173572ad28c72032598ce
xlyann <gdhfufigj@gmail.com>
2024-09-08 21:11:16 +0900

    Title: 전부 겹치는 선분 2, Time: 138ms, Memory: 25MB, Status: Passed - CodeTree

--- 240908/전부 겹치는 선분 2/overlapping-line-segments-2.py
+++ 240908/전부 겹치는 선분 2/overlapping-line-segments-2.py
@@ -11,7 +11,7 @@ for i in range(n):
         x1, x2 = line[j][0], line[j][1]
         max_x1 = max(max_x1, x1)
         min_x2 = min(min_x2, x2)
-    if max_x1 < min_x2:
+    if max_x1 <= min_x2:
         overlap = True
 
 if overlap:
