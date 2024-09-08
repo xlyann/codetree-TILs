e0006adc2c30316f35377c0b69467083279d6679
xlyann <gdhfufigj@gmail.com>
2024-09-08 20:48:17 +0900

    Title: 전부 겹치는 선분 2, Time: 152ms, Memory: 25MB, Status: Wrong Answer - CodeTree

---
+++ 240908/전부 겹치는 선분 2/overlapping-line-segments-2.py
@@ -0,0 +1,19 @@
+n = int(input())
+line = list(tuple(map(int, input().split())) for _ in range(n))
+
+overlap = False
+for i in range(n):
+    max_x1 = 0
+    min_x2 = float('inf')
+    for j in range(n):
+        if i == j:
+            continue
+        x1, x2 = line[j][0], line[j][1]
+        max_x1 = max(max_x1, x1)
+        min_x2 = min(min_x2, x2)
+    if max_x1 > min_x2:
+        overlap = True
+if overlap:
+    print('Yes')
+else:
+    print('No')
