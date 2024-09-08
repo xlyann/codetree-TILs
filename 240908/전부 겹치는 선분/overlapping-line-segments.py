c66de4066254e78e66c8e76522a8744ccd18ea46
xlyann <gdhfufigj@gmail.com>
2024-09-08 20:29:58 +0900

    Title: 전부 겹치는 선분, Time: 145ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240908/전부 겹치는 선분/overlapping-line-segments.py
@@ -0,0 +1,16 @@
+n = int(input())
+line = list(tuple(map(int, input().split())) for _ in range(n))
+
+ans = 0
+for i in range(1, 101):
+    overlap = True
+    for a, b in line:
+        if a > i or i < b:
+            overlap = False
+    if overlap:
+        ans = 1
+
+if ans == 1:
+    print('Yes')
+else:
+    print('No')
