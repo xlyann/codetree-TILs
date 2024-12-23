0de09408d34c9cd8b55724888cb32bf771c840db
xlyann <gdhfufigj@gmail.com>
2024-12-23 16:02:59 +0900

    Title: 코딩톡, Time: 46ms, Memory: 16MB, Status: Wrong Answer - CodeTree

--- 241223/코딩톡/coding-talk.py
+++ 241223/코딩톡/coding-talk.py
@@ -3,14 +3,17 @@ people = []
 for i in range(n):
     people.append(chr(65+i))
 
+save = list(list(input().split()) for _ in range(m))
+
 for i in range(m):
-    who, read = input().split()
     if i < p-1:
         continue
-    if i == p-1 and read == '0':
+    if i == p-1 and save[i][1] == '0':
         people.clear()
         break
-    if who in people:
-        people.remove(who)
+    if i == p-1 and i != 0 and save[i][1] == save[i-1][1]:
+        people.remove(save[i-1][0])
+    if save[i][0] in people:
+        people.remove(save[i][0])
 
 print(*people)
