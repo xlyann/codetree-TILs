946f5df08885efc310525c393fa833e681722bf8
xlyann <gdhfufigj@gmail.com>
2024-12-23 16:52:16 +0900

    Title: X 달리기, Time: 46ms, Memory: 16MB, Status: Passed - CodeTree

--- 241223/X 달리기/x-run.py
+++ 241223/X 달리기/x-run.py
@@ -1,7 +1,6 @@
 meter = int(input())
-start = 1
 
-for i in range(1, 100):
+for i in range(1, 102):
     if meter < i*i:
         value = i-1
         break
@@ -9,4 +8,4 @@ for i in range(1, 100):
 if meter == value**2:
     print(2*value-1)
 else:
-    print(value*2)
+    print(value*2+(meter-value**2)//value)
